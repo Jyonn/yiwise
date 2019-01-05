d90e7c872047d439a7ec655ce53de9b05257e2c5
adelliu <lqj679ssn@qq.com>
2019-01-05 11:36:17 +0800

    优化

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, json
 
 app = Flask(__name__)
 
 
-@app.route('/callback')
+@app.route('/callback', methods=['GET', 'POST'])
 def callback():
-    print(request.form)
-    print(request.args)
-    return ''
+    call_id = request.form['call_id']
+    print(json.loads(request.form['content'])['words'])
+    result = {
+        "result": "success",
+        "msg": "成功",
+        "code": 200,
+        "data": {
+            "call_id": call_id,
+            "action": "play",
+            "action_code": "a5a86093-2dec-4992-b2af-8db53e3f8e35.wav",
+        }
+    }
+    return jsonify(result)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
